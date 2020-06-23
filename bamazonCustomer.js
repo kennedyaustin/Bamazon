0a80cd4991ebd18c1bd1f8fced5542eb9b2bc34a
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-06-23 00:12:20 -0400

    making it so the user can see the inventory as well as order from the list they see before them at the same time, give them their total amount due to the store

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,63 +1,94 @@
+// Requiring packages
 const inquirer= require('inquirer')
 const mysql= require('mysql2')
 
 var connection = mysql.createConnection({
     host: "localhost",
   
-    // Your port; if not 3306
     port: 3306,
   
-    // Your username
     user: "root",
   
-    // Your password
     password: "Shadow4592",
     database: "bamazon"
 });
 
-// This function will handle the inventory that is shown to the consumer before they are prompted to 
-// choose an item id to add to their cart
-function displayInventory() {
+// ---------------------------------- Inventory -------------------------------------------
 
-    inventory= 'select * from products'
+function storeInventory() {
+
+    let inventory= 'select * from products'
     connection.query(inventory, function(error, product) {
 
         if (error) throw error
-
-        console.log('Welcome to Greenix! Please browse our wares before making your decision.')
+        // This for loop will grab the item id, product name, and price before showing it to the 
+        // user in a table in the terminal
+        console.log('\n---------------------- Please browse our wares before making your decision. ----------------------\n')
         for (let i = 0; i < product.length; i++) {
 
             console.table(`Item ID: ${product[i].item_id}  |  Item: ${product[i].product_name}  |  Price: ${product[i].price}`)
 
         }
-
     })
-
+    whatUserWants()
 }
+storeInventory()
 
-displayInventory();
-// function userBuying() {
+// ----------------------------------- Prompt User to Buy Something -------------------------------------
 
-//     inquirer
-//   .prompt([
-//     {
-//         type: 'list',
-//         name: 'buyItem',
-//         message: 'Welcome to Greenix! Please browse our items before making your choice.',
-        
-//         // validate: validateInput,
-//         // filter: Number
-//     }
-//   ])
-//   .then(answers => {
-//     // Use user feedback for... whatever!!
-//   })
-//   .catch(error => {
-//     if(error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else when wrong
-//     }
-//   });
+function whatUserWants() {
 
-// }
+    inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'itemID',
+        message: 'Please enter the item ID for the item you would like to purchase!',
+        // This will make sure that the user has to type a number into the terminal
+        validate: function(value) {
+
+            if (isNaN(value) === false) {
+                return true
+            } else {
+                return false 
+            }
+        }
+    }, {
+        type: 'input',
+        name: 'numberofUnits',
+        message: 'How many of this item would you like to buy?',
+        validate: function(value) {
+
+            if (isNaN(value) === false) {
+                return true
+            } else {
+                return false 
+            }
+        }
+    }
+  ])
+  .then(userInput => {
+    
+    // This will grab the items that the user chooses and run it through the for loop below
+    connection.query('select * from products where item_id= ?', userInput.itemID, function(error, product) {
+
+        // For each product the user chooses, they will be shown one of the two messages shown inside the if statement
+        for (let i = 0; i < product.length; i++) {
+
+            // If the user orders more than the store currently has in stock the will be shown this message
+            if (userInput.numberofUnits > product[i].stock_quantity) {
+
+                console.log('\n----------- Sorry for the inconvenience, we do not have enough inventory in stock to fulfill your order!-----------\n')
+
+            //  If the store has enough in stock of the item the user chooses, they will be shown this information
+            } else {
+
+
+                console.log('\nYou have selected: \n')
+                console.log(`Item: ${product[i].product_name}\nQuantity: ${userInput.numberofUnits}\nPrice per item: $${product[i].price}\nTotal: $` + (product[i].price * userInput.numberofUnits))
+
+            }
+        }
+    })
+  })
+}
