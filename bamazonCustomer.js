9ebcc27cc6e94d40dee2bdbad24eda3cab675672
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-06-22 22:59:30 -0400

    had to fix database name from bamzon to bamazon, and made it so the inventory is shown to the consumer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,3 +14,50 @@ var connection = mysql.createConnection({
     password: "Shadow4592",
     database: "bamazon"
 });
+
+// This function will handle the inventory that is shown to the consumer before they are prompted to 
+// choose an item id to add to their cart
+function displayInventory() {
+
+    inventory= 'select * from products'
+    connection.query(inventory, function(error, product) {
+
+        if (error) throw error
+
+        console.log('Welcome to Greenix! Please browse our wares before making your decision.')
+        for (let i = 0; i < product.length; i++) {
+
+            console.table(`Item ID: ${product[i].item_id}  |  Item: ${product[i].product_name}  |  Price: ${product[i].price}`)
+
+        }
+
+    })
+
+}
+
+displayInventory();
+// function userBuying() {
+
+//     inquirer
+//   .prompt([
+//     {
+//         type: 'list',
+//         name: 'buyItem',
+//         message: 'Welcome to Greenix! Please browse our items before making your choice.',
+        
+//         // validate: validateInput,
+//         // filter: Number
+//     }
+//   ])
+//   .then(answers => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch(error => {
+//     if(error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else when wrong
+//     }
+//   });
+
+// }
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -1,9 +1,9 @@
 drop database if exists bamazon;
-create database bamzon;
+create database bamazon;
 use bamazon;
 
 create table products (
-id int auto_increment primary key,
+item_id int auto_increment primary key,
 product_name varchar(100) not null,
 department_name varchar(75) not null,
 price decimal(10,2) not null,
